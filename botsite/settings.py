0795011db93692c6c43d2b27db21d6e8cb190079
Vashishth Patel <vashishthchaudhary48@gmail.com>
2022-03-04 16:41:05 +0530

    media files added

--- botsite/settings.py
+++ botsite/settings.py
@@ -118,6 +118,16 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR,'static')
+]
+
+
+MEDIA_URL='/media/'
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+STATIC_ROOT=os.path.join(BASE_DIR,'assets')
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
