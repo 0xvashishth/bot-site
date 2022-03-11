ac3c381a72726b6923dc1a3dd3d7e7c472afb37f
Darshit <darshitbhuva9330@gmail.com>
2022-03-11 17:51:07 +0530

    fix

--- bot/urls.py
+++ bot/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('register', views.register, name="register"),
     path('login/', views.loginuser, name="loginuser"),
     path('registeruser/', views.registeruser, name="registeruser"),
-    path("userprofile", views.userprofile, name="viewprofile")
+    path("userprofile/<username>", views.userprofile, name="viewprofile")
 ] 
