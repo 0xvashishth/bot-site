27190782233cddb1d5af3b17bd296383a77eab44
Darshit <darshitbhuva9330@gmail.com>
2022-03-15 18:08:04 +0530

    changed

--- botsite/settings.py
+++ botsite/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'botsite',
         'USER': 'postgres',
-        'PASSWORD': '2021',
+        'PASSWORD': 'bhuva',
         'HOST': '127.0.0.1'
         
     }
