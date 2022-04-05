333c01cf69d791b8a5dbe11add9128912bc75962
Darshit <darshitbhuva9330@gmail.com>
2022-04-05 17:38:34 +0530

    edit about

--- botsite/settings.py
+++ botsite/settings.py
@@ -130,8 +130,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
-EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
+EMAIL_HOST_USER = 'vasutemporarylc@gmail.com'
+EMAIL_HOST_PASSWORD = 'vurioxlxtzfheorj'
 
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
